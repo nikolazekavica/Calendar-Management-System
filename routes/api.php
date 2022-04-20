7a2c4c87a3fa41b99d55e522e986f23c53163aeb
Nikola Zekavica <n.zekavica@ldc.local>
2022-04-20 13:41:21 +0200

    update code

--- routes/api.php
+++ routes/api.php
@@ -16,21 +16,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-//admin
-Route::get("availabilities", [AvailabilityController::class, "all"])->middleware(['auth:api','scopes:admin']);
-Route::get("/users/filter/availabilities", [AvailabilityController::class, "allByUser"])->middleware(['auth:api','scopes:admin']);
-Route::get("availabilities/filter", [AvailabilityController::class, "allByDateRange"])->middleware(['auth:api','scopes:admin']);
 
-//regular
-Route::get("users/{id}/availabilities", [AvailabilityController::class, "allByUserId"])->middleware(['auth:api','id.verified']);
+//AVAILABILITY ROUTES
+Route::get("availabilities", [AvailabilityController::class, "all"])->middleware(['auth:api','scopes:admin']);
+Route::get("availabilities/filter", [AvailabilityController::class, "allByDateRange"])->middleware(['auth:api','scopes:admin']);
 Route::post("availabilities", [AvailabilityController::class, "store"])->middleware(['auth:api','scopes:regular']);
 
-//all
+//USERS ROUTES
+Route::get("users/filter/availabilities", [AvailabilityController::class, "allByUser"])->middleware(['auth:api','scopes:admin']);
+Route::get("users/{id}/availabilities", [AvailabilityController::class, "allByUserId"])->middleware(['auth:api','id.verified']);
 Route::post("users/register", [RegistrationController::class, "register"])->middleware('db.transaction');
 Route::post("users/login", [LoginController::class, "login"]);
 Route::post("users/logout", [LoginController::class, "logout"])->middleware(['auth:api']);;
 Route::get("users/verify", [RegistrationController::class, "verify"]);
-
 Route::get("users/roles", [RoleController::class, "all"])->middleware(['auth:api']);
 
 
